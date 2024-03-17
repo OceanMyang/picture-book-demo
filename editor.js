a791964c862675b6cde7aec6531c00e7dd18fe5e
OceanZ <my18702189114@gmail.com>
2024-03-17 13:11:40 -0700

    loading small

--- editor.js
+++ editor.js
@@ -19,7 +19,7 @@ $("#add-link").on("click", async ()=>{
     if (range.startContainer === range.endContainer) {
       const anchor = $('<a/>', {
         class: "view",
-        href: "images/loading.gif"})
+        href: "images/loading-sm.gif"})
         .css({
           display: "inline-block",
           // pointerEvents: "none",
